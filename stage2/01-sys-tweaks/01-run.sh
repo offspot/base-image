78baeec6dd110b3c274788beab2bbcebf6215b74
Florian HESLOUIN <florian.heslouin@bibliosansfrontieres.org>
2021-11-26 14:39:50 +0100

    Enable python3 and pip3 as default. Force install Ansible with python3

--- stage2/01-sys-tweaks/01-run.sh
+++ stage2/01-sys-tweaks/01-run.sh
@@ -61,4 +61,11 @@ on_chroot << EOF
 usermod --pass='*' root
 EOF
 
+on_chroot << EOF
+update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
+update-alternatives --install /usr/bin/python python /usr/bin/python3.7 2
+update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 1
+pip3 install ansible==2.9.27
+EOF
+
 rm -f "${ROOTFS_DIR}/etc/ssh/"ssh_host_*_key*
