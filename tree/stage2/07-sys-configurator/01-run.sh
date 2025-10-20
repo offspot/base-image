e3f2719a0dcacc115dc4c10cb04da4d9fa416507
rgaudin <reg@rskg.org>
2025-10-20 09:19:30 +0000

    Using offspot-config 2.7.1

--- tree/stage2/07-sys-configurator/01-run.sh
+++ tree/stage2/07-sys-configurator/01-run.sh
@@ -2,8 +2,8 @@
 
 on_chroot << EOF
 python3 -m venv /usr/local/offspot-python
-/usr/local/offspot-python/bin/pip install https://github.com/offspot/offspot-config/archive/refs/heads/main.zip
-#/usr/local/offspot-python/bin/pip install offspot-config==2.7.0
+#/usr/local/offspot-python/bin/pip install https://github.com/offspot/offspot-config/archive/refs/heads/main.zip
+/usr/local/offspot-python/bin/pip install offspot-config==2.7.1
 EOF
 
 install -m 755 files/offspot-runtime.service       "${ROOTFS_DIR}/etc/systemd/system/"
